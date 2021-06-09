20d0c9455279d776a9014b00e7e9cf06a6752925
Aada21 <mohamedhassa20@gmail.com>
2021-06-09 04:00:31 +0200

    Drugs

--- lib/Diabetes.dart
+++ lib/Diabetes.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:imagebutton/imagebutton.dart';
 import 'chatbot.dart';
-
+import 'package:intl/intl.dart';
 class Diabetes extends StatefulWidget {
   static String id = 'Diabetes_page';
   const Diabetes({Key key}) : super(key: key);
@@ -13,11 +13,243 @@ class Diabetes extends StatefulWidget {
 }
 
 class _DiabetesState extends State<Diabetes> {
+  List<String> _items = [];
+  Animation _animation;
+  final key = GlobalKey<AnimatedListState>();
+  String _setDate='';
+  String _dis='';
+  TextEditingController _dateController = TextEditingController();
+  DateTime selectedDate = DateTime.now();
+  //fun.of date.
+  Future<Null> _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        firstDate: DateTime(2000),
+        lastDate: DateTime(2101));
+    if (picked != null) {
+      setState(() {
+        selectedDate = picked;
+        _dateController.text = DateFormat.yMMMM('en_US').format(selectedDate);
+      }
+      );
+    }
+  }
   @override
   Widget build(BuildContext context) {
     var mediaQueryData = MediaQuery.of(context);
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
+
+
+    void showDialog() {
+      //it show dialog
+      showGeneralDialog(
+        barrierLabel: "Barrier",
+        barrierDismissible: true,
+        barrierColor: Colors.black.withOpacity(0.5),
+        transitionDuration: Duration(milliseconds: 700),
+        context: context,
+        pageBuilder: (_, __, ___) {
+          return Dialog(
+              backgroundColor: Colors.green,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0)),
+              child: SizedBox(
+                height: screenHeight - 190,
+                width: screenWidth / 2,
+                child: Container(
+                    height: screenHeight,
+                    width: screenHeight,
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: SingleChildScrollView(
+                      child: Stack(
+                        children: [
+                          Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding:
+                                  const EdgeInsets.fromLTRB(60, 15, 10, 0),
+                                  child: Text(
+                                    'Enter Medicines ',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16.0,
+                                      letterSpacing: 2.0,
+                                      decoration: TextDecoration.none,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding:
+                                  const EdgeInsets.fromLTRB(10, 30, 10, 0),
+                                  child: Text(
+                                    'Enter your Medicine  :',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16.0,
+                                      letterSpacing: 2.0,
+                                      decoration: TextDecoration.none,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding:
+                                  const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                                  child: TextField(
+                                    // inputFormatters: <TextInputFormatter>[
+                                    //   FilteringTextInputFormatter.allow(RegExp("[a-z]")),
+                                    // ],
+                                    onChanged: (value){
+                                      _dis=value.trim();
+                                    },
+                                    decoration: InputDecoration(
+                                        enabledBorder: UnderlineInputBorder(
+                                          borderSide:
+                                          BorderSide(color: Colors.white),
+                                          borderRadius:
+                                          BorderRadius.circular(10),
+                                        ),
+                                        border: InputBorder.none,
+                                        filled: true,
+                                        fillColor: Colors.white,
+                                        focusedBorder: OutlineInputBorder(
+                                            borderRadius:
+                                            BorderRadius.circular(10),
+                                            borderSide: BorderSide(color: Colors.white)
+                                        ),
+                                        hintText: ('Medicine...'),
+                                        labelStyle: TextStyle(
+                                            color: Colors.black, fontSize: 20),
+                                        hintStyle:
+                                        TextStyle(color: Colors.black)),
+                                  ),
+                                ),
+                                Padding(
+                                  padding:
+                                  const EdgeInsets.fromLTRB(10, 15, 10, 0),
+                                  child: Text(
+                                    'Date :',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16.0,
+                                      letterSpacing: 2.0,
+                                      decoration: TextDecoration.none,
+                                      height: 5,
+                                    ),
+                                  ),
+                                ),
+                                Column(
+                                  children: <Widget>[
+                                    Text(
+                                      'Choose your Date',
+                                      style: TextStyle(
+                                          fontStyle: FontStyle.italic,
+                                          fontWeight: FontWeight.w600,
+                                          letterSpacing: 0.5,
+                                          color:Colors.white ),
+                                    ),
+                                    InkWell(
+                                      onTap: ()
+                                      {
+                                        _selectDate(context);
+                                      },
+                                      child:
+                                      Container(
+                                        width: screenWidth / 1.3,
+                                        height: screenHeight / 15,
+                                        margin: EdgeInsets.only(top: 7),
+                                        alignment: Alignment.center,
+                                        decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(10.0),
+                                        ),
+                                        child: TextFormField(
+                                          style: TextStyle(fontSize: 40),
+                                          textAlign: TextAlign.center,
+                                          enabled: false,
+                                          keyboardType: TextInputType.text,
+                                          controller: _dateController,
+                                          onSaved: (String val) {
+                                            _setDate = val;
+                                          },
+                                          decoration: InputDecoration(
+                                              hintText:DateFormat.yMMMM('en_US').format(selectedDate),
+                                              disabledBorder:
+                                              UnderlineInputBorder(borderSide: BorderSide.none),
+                                              // labelText: 'Time',
+                                              contentPadding: EdgeInsets.only(top: 0.0)
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                //date here
+                                Row(
+                                  mainAxisAlignment:
+                                  MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.fromLTRB(
+                                          25, 110, 10, 0),
+                                      child: ElevatedButton(
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                        style: ElevatedButton.styleFrom(
+                                            primary: Colors.red,
+                                            onPrimary: Colors.black,
+                                            shape: RoundedRectangleBorder(
+                                              borderRadius:
+                                              BorderRadius.circular(20.0),
+                                            )),
+                                        child: Text('Cancel'),
+                                      ),
+                                    ),
+
+                                    Padding(
+                                      padding: const EdgeInsets.fromLTRB(
+                                          30, 110, 30, 0),
+                                      child: ElevatedButton(
+                                        onPressed: () {
+                                          if(_dis.isNotEmpty ){
+                                            addItem(_dis);
+                                            Navigator.pop(context);
+                                            _dis='';
+                                          }
+                                        },
+                                        style: ElevatedButton.styleFrom(
+                                            primary: Colors.green,
+                                            onPrimary: Colors.black,
+                                            shape: RoundedRectangleBorder(
+                                              borderRadius:
+                                              BorderRadius.circular(20.0),
+                                            )),
+                                        child: Text('Submit'),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ]),
+                        ],
+                      ),
+                    )),
+              ));
+        },
+        transitionBuilder: (_, anim, __, child) {
+          return SlideTransition(
+            position:
+            Tween(begin: Offset(0, 1), end: Offset(0, 0)).animate(anim),
+            child: child,
+          );
+        },
+      );
+    }
+
     return Scaffold(
       body: SafeArea(
         child: Center(
@@ -28,7 +260,7 @@ class _DiabetesState extends State<Diabetes> {
               gradient: LinearGradient(
                   begin: Alignment.bottomLeft,
                   end: Alignment.topRight,
-                  colors: [Colors.blueGrey.withOpacity(.3), Colors.white60]),
+                  colors: [Colors.blue[200].withOpacity(.3), Colors.white60]),
             ),
             child: Stack(children: <Widget>[
               Container(
@@ -45,7 +277,7 @@ class _DiabetesState extends State<Diabetes> {
                         Navigator.pop(context);
                       },
                     ),
-                    Text("HOME", //Home word
+                    Text("Drugs", //Home word
                         style: TextStyle(
                           fontSize: 18,
                           color: Colors.black,
@@ -54,7 +286,9 @@ class _DiabetesState extends State<Diabetes> {
                       icon: Icon(Icons.edit),
                       color: Colors.black,
                       iconSize: 20.0,
-                      onPressed: () {},
+                      onPressed: () {
+                        showDialog();
+                      },
                     ),
                   ],
                 ),
@@ -67,42 +301,20 @@ class _DiabetesState extends State<Diabetes> {
                   double localWidth = constrains.maxWidth;
 
                   return Container(
-                    //The central container in which the text will be located
-                    width: localWidth - 85,
-                    height: localHeight - 190,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(color: Colors.grey, spreadRadius: 1),
+                    width: localWidth-85,height: localHeight-180,
+                    child: Column(
+                      children: [
+                        Expanded(
+                            child:
+                            AnimatedList(
+                              key: key,
+                              initialItemCount: _items.length,
+                              itemBuilder: (context, int index, Animation<double> animation) {
+                                return buildItem(_items[index], animation, index);
+                              },
+                            )
+                        )
                       ],
-                    ),
-                    child: SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          TextField(
-                            //The text field to which the medical history will be added
-                            keyboardType: TextInputType.multiline,
-                            maxLines: 15,
-                            decoration: InputDecoration(
-                              contentPadding: //EdgeInsets.fromLTRB(15, 222, 15, 2),
-                                  EdgeInsetsDirectional.fromSTEB(
-                                      10, 124, 30, 208),
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                  borderSide: BorderSide(
-                                    width: 1,
-                                    color: Colors.white,
-                                  )),
-                              focusColor: Colors.white60,
-                              focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              hintText: 'Diabetes',
-                            ),
-                          ),
-                        ],
-                      ),
                     ),
                   );
                 }),
@@ -152,6 +364,41 @@ class _DiabetesState extends State<Diabetes> {
         ),
       ),
     );
+  }
+  Widget buildItem( _dis, Animation animation,  index) {
+    return ScaleTransition(
+      scale: animation,
+      child: Card(
+        child: ListTile(
+          title: Text(
+            "$_dis",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          trailing: IconButton(
+            onPressed: () {
+              removeItem(index);
+            },
+            icon: Icon(
+              Icons.close,
+              color: Colors.red,
+            ),
+          ),
+
+        ),
+      ),
+    );
+  }
+  removeItem(int index) {
+    String removeItemed = _items.removeAt(index);
+    AnimatedListRemovedItemBuilder builder = (context, animation) {
+      return buildItem(removeItemed, animation, index);
+    };
+    key.currentState.removeItem(index, builder);
+  }
+  void addItem(String x) {
+    int i = _items.length > 0 ? _items.length : 0;
+    _items.insert(i, "$_dis \n${DateFormat.MMMM().format(selectedDate)}/${selectedDate.year}");
+    key.currentState.insertItem(i);
   }
 }
 
